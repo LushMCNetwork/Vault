0ae940cae7c04bc7c855d00ab0d442bafc2d2a9d
Josiah Royal <119104108+mechobliterate@users.noreply.github.com>
2026-09-08 02:58:46 -0400

    Support Folia-based server forks

--- src/net/milkbowl/vault/Vault.java
+++ src/net/milkbowl/vault/Vault.java
@@ -1,17 +1,15 @@
 package net.milkbowl.vault;
 
-import io.papermc.paper.ServerBuildInfo;
-import net.kyori.adventure.key.Key;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Vault extends JavaPlugin {
 
-    private static final Key FOLIA = Key.key("papermc", "folia");
-
     @Override
     public void onEnable() {
-        if (!ServerBuildInfo.buildInfo().isBrandCompatible(FOLIA)) {
-            throw new IllegalStateException("Vault requires Folia 26.1.2");
+        try {
+            Class.forName("io.papermc.paper.threadedregions.RegionizedServer", false, getClassLoader());
+        } catch (ClassNotFoundException exception) {
+            throw new IllegalStateException("Vault requires a Folia-based server", exception);
         }
 
         getLogger().info("Enabled " + getPluginMeta().getVersion());
